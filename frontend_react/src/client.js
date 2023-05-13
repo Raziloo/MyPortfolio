c568418d94479ba51d2b62c2031772f3c6b91788
Raziloo <waissraz27@gmail.com>
2023-05-13 17:38:58 +0300

    inital files

---
+++ frontend_react/src/client.js
@@ -0,0 +1,16 @@
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+const token = process.env.REACT_APP_SANITY_TOKEN;
+
+export const client = createClient({
+   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+   dataset: 'production',
+   apiVersion: '2023-05-01',
+   useCdn: true,
+   token: token, 
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
